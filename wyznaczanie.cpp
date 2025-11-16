76ae6a688010f14cf603238737fbb09f5a9cdd93
Alfons <148062342+alfonsmucha@users.noreply.github.com>
2025-11-16 23:52:35 +0100

    Update wyznaczanie.cpp

--- wyznaczanie.cpp
+++ wyznaczanie.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 { 
-    int n = 0;
-    int d = 0;
+    int n = 0;  //deklaracja zmiennej n, ktora bedziemy sprawdzac czy jest liczba pierwsza
+    int d = 0;  //d przechowujaca dzielniki od 2
     
-    cout<<"Podaj liczbe: ";  //funkcja
-    cin>> n;                                  //funkcja
+    cout<<"Podaj liczbe: ";  
+    cin>> n;                                 
     
     if (n < 2) 
     {
@@ -16,7 +16,7 @@ int main()
         return 0;
     }
     
-    for (d = 2; d <= sqrt(n); d++) 
+    for (d = 2; d <= sqrt(n); d++)    //sprawdzamy dzielniki tylko do √n, jeśli liczba ma dzielnik większy niż √n, to drugi musi być mniejszy niż √n
     {
         if (n % d == 0)
         {
